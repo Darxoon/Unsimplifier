8bebf9ac31f5d679d3437084b75c107b5718ffa4
Darxoon <darxoon@outlook.com>
2026-03-01 21:59:49 +0100

    Update dependencies

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import preprocess from 'svelte-preprocess';
+import { sveltePreprocess } from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 const isDevVersion = !!parseInt(process.env.PUBLIC_IS_DEV_VERSION ?? "1")
@@ -7,7 +7,7 @@ const isDevVersion = !!parseInt(process.env.PUBLIC_IS_DEV_VERSION ?? "1")
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: sveltePreprocess(),
 
 	kit: {
 		paths: {
